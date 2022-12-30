72d9f9820981a1e9cf90857bb3979bb6061f0bac
brandonszeto <brandonszeto@gmail.com>
2022-12-30 14:22:02 -0800

    Changes to get_config_path

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,23 @@
 use anyhow::{Error, Result};
 use serde::{Deserialize, Serialize};
-use std::io::stdin;
+use std::{
+    fs,
+    io::stdin,
+    path::{Path, PathBuf},
+};
+
+const CONFIG_DIR: &str = ".config";
+const APP_CONFIG_DIR: &str = "todo";
+const CONFIG_FILE: &str = "config.json";
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct UserConfig {
-    token: String,
-    color: bool,
+    pub token: String,
+    pub color: bool,
+}
+
+pub struct ConfigPath {
+    pub config_file_path: PathBuf,
 }
 
 impl UserConfig {
@@ -16,8 +28,36 @@ impl UserConfig {
         }
     }
 
+    pub fn get_config_path(&self) -> Result<ConfigPath> {
+        match dirs::home_dir() {
+            Some(home) => {
+                let home_path = Path::new(&home);
+                let config_dir = home_path.join(CONFIG_DIR);
+                let app_config_dir = config_dir.join(APP_CONFIG_DIR);
+                if !config_dir.exists() {
+                    fs::create_dir(&config_dir)?;
+                }
+
+                if !app_config_dir.exists() {
+                    fs::create_dir(&app_config_dir)?;
+                }
+
+                let config_file_path = &app_config_dir.join(CONFIG_FILE);
+
+                let final_path = ConfigPath {
+                    config_file_path: config_file_path.to_path_buf(),
+                };
+                Ok(final_path)
+            }
+            None => Err(Error::from(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                format!("No $HOME directory found for config file"),
+            ))),
+        }
+    }
+
     // Loads api token from cache or config, requests if not found
-    pub fn load_api_token() {}
+    pub fn load_config() {}
 
     // Requests and saves user input api token
     pub fn get_api_token() -> Result<String> {
